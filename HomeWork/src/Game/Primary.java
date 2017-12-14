0daa14c265b60c3f00b6c1f9d529335424958656
learnen <1372892517@qq.com>
2017-12-14 11:53:37 +0800

    1

--- HomeWork/src/Game/Primary.java
+++
@@ -1,9 +0,0 @@
-package Game;
-
-public class Primary {
-
-    public void play(){
-        
-    }
-
-}
---
+++ HomeWork/src/Main2.java
@@ -0,0 +1,9 @@
+import Game.Game;
+
+public class Main2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Game.play();
+    }
+
+}
